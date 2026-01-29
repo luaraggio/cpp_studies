16bd1b5d0186a6f0732f766b132fbc71bb309d41
Luara Raggio <luararaggio@hotmail.com>
2026-01-28 22:22:37 -0300

    fix(CPP09): change argc argv condition in cpp01

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -6,17 +6,18 @@
 /*   By: lraggio <lraggio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 10:18:10 by lraggio           #+#    #+#             */
-/*   Updated: 2026/01/21 15:43:26 by lraggio          ###   ########.fr       */
+/*   Updated: 2026/01/28 22:22:12 by lraggio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 int	main(int argc, char **argv) {
-	if (argc < 2) {
+	if (argc != 2) {
 		std::cerr << "Error: Wrong arguments. It must be: ./RPN <numbers> <operations>." << std::endl;
 		return (-1);
 	}
+
 	try {
 		RPN rpn;
 		rpn.performRPN(argv[1]);
